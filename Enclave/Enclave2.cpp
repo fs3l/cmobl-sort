47d48f055d70374f03a63aab788c439b7e0e5636
Yuzhe Tang <tristartom@gmail.com>
2017-09-06 12:51:06 -0400

    new
    Signed-off-by: Yuzhe Tang <tristartom@gmail.com>

--- Enclave/Enclave2.cpp
+++ Enclave/Enclave2.cpp
@@ -42,26 +42,45 @@ extern "C" {
   }
 
 static 
-inline void tx_begin(int32_t* data, int32_t size) {
-  for (int j=0;j<size;j++)
-      data[j]=5;
- asm volatile("mov %0, %%edx\n\t"::"r"(size):"%edx");
- asm volatile("mov $0, %%eax\n\t":::"%eax");
- asm volatile("mov %0, %%rcx\n\t"::"m"(data):"%rcx");
+inline void tx_begin2(int32_t** data, int32_t* sizes, int32_t size) {
+  bar1("xbegin, %p,%d\n",data,size);
+  for (int j=0;j<size;j++){
+    for (int i = 0; i < sizes[j]; i++)
+      data[j][i]=(data[j][i] + 1) - 1;//TODO check dirty lines?
+  }
   __asm__("xbegin asm_abort_handler");
- // for (int j=0;j<size;j++)
- //     data[j]=0;
- //data prefetch phase
-  asm volatile(
-        "loop:cmpl  %%edx,%%eax\n\t"
-	"jge	endloop\n\t"
-	"movl	$5, (%%rcx)\n\t"
-	"addl	$1, %%eax\n\t"
+  for (int j=0;j<size;j++){
+    for (int i = 0; i < sizes[j]; i++)
+      data[j][i]=(data[j][i] + 1) - 1;//TODO check dirty lines?
+  }
+}
+
+static 
+inline void tx_begin(int32_t* data, int32_t size) {
+  //for (int j=0;j<size;j++)
+    //  data[j]=5;//data[j]+1-1; //TODO check if causing dirty lines?
+asm volatile("mov %0, %%edx\n\t"
+        "mov $0, %%eax\n\t"
+        "mov %1, %%rbx\n\t"
+        "mov %%rbx,%%rcx\n\t"
+        "loop1:cmpl  %%edx,%%eax\n\t"
+        "jge    endloop1\n\t"
+        "movl   $5, (%%rcx)\n\t"
+        "addl   $1, %%eax\n\t"
         "add   $4, %%rcx\n\t"
-	"jmp	loop\n\t"
+        "jmp    loop1\n\t"
+        "endloop1:\n\t"
+        "xbegin asm_abort_handler\n\t"
+        "mov %%rbx,%%rcx\n\t"
+        "loop:cmpl  %%edx,%%eax\n\t"
+        "jge    endloop\n\t"
+        "movl   $5, (%%rcx)\n\t"
+        "addl   $1, %%eax\n\t"
+        "add   $4, %%rcx\n\t"
+        "jmp    loop\n\t"
         "endloop:\n\t"
         :
-        :
+        :"r"(size),"r"(data)
         :"rcx","eax","edx");
 }
 static 
@@ -78,6 +97,15 @@ void compute_CPU3_distribute(int32_t* E_data, int32_t* E_perm, int32_t* E_output
   int32_t inter2[N*BLOWUPFACTOR];
   for (int j=0; j<SqrtN; j++){
     tx_begin(inter1,SqrtN*BLOWUPFACTOR);
+
+/**
+  int32_t inter1[SqrtN*BLOWUPFACTOR];
+  int32_t inter2[N*BLOWUPFACTOR];
+  int32_t* data[1]; data[0] = inter1;
+  int32_t sizes[1]; sizes[0] = SqrtN*BLOWUPFACTOR;
+  for (int j=0; j<SqrtN; j++){
+    tx_begin2(data, sizes, 1);
+*/
 //    for (int i=0;i<SqrtN;i++)
 //      inter1[E_perm[i]] = E_data[i];
     tx_end();
