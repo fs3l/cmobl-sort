9268319a6a7790acd9c8a54db5fa5e9bc2e95760
Xu Cheng <git@xuc.me>
2017-10-25 18:06:52 -0400

    sort: partition write ob

--- Enclave/sort_real.cpp
+++ Enclave/sort_real.cpp
@@ -23,9 +23,17 @@ public:
     data[0] = max_len;
     data[1] = 0;
     data[2] = 0;
-    nob = initialize_nob_array(data, max_len + 3);
   }
   ~Queue() { delete[] data; }
+  __attribute__((always_inline)) inline void init_nob()
+  {
+    nob = initialize_nob_array(data, data[0] + 3);
+  }
+  __attribute__((always_inline)) inline void reset_nob()
+  {
+    for (int32_t i = 1; i < data[0] + 3; ++i) data[i] = nob_read_at(nob, i);
+  }
+
   __attribute__((always_inline)) inline int32_t get_max_len() const
   {
     return nob_read_at(nob, 0);
@@ -82,6 +90,32 @@ private:
   HANDLE nob;
 };
 
+static __attribute__((always_inline)) inline void reset_tx(
+    Queue* lhs_q, Queue* rhs_q, int32_t* lhs_out, int32_t* rhs_out,
+    const int32_t lhs_len, const int32_t rhs_len, const int32_t lhs_out_idx,
+    const int32_t rhs_out_idx, int32_t* arr_out, const int32_t len,
+    const int32_t arr_out_idx, HANDLE* lhs_out_ob, HANDLE* rhs_out_ob,
+    HANDLE* arr_out_ob, int32_t* ob_write, int32_t* max_ob_write)
+{
+  coda_txend();
+  lhs_q->reset_nob();
+  rhs_q->reset_nob();
+  for (int32_t i = 0; i < *ob_write; i++)
+    arr_out[arr_out_idx - *ob_write + i] = ob_rw_read_next(*arr_out_ob);
+
+  *lhs_out_ob =
+      initialize_ob_iterator(lhs_out + lhs_out_idx, lhs_len - lhs_out_idx);
+  *rhs_out_ob =
+      initialize_ob_iterator(rhs_out + rhs_out_idx, rhs_len - rhs_out_idx);
+  *arr_out_ob =
+      initialize_ob_rw_iterator(arr_out + arr_out_idx, len - arr_out_idx);
+  lhs_q->init_nob();
+  rhs_q->init_nob();
+  *ob_write = 0;
+  *max_ob_write = 1;  // TODO: API to get max ob write number
+  coda_txbegin();
+}
+
 void merge_sort(const int32_t* arr_in, int32_t* arr_out, const size_t len)
 {
   if (len == 1) {
@@ -104,28 +138,45 @@ void merge_sort(const int32_t* arr_in, int32_t* arr_out, const size_t len)
   size_t half_s = s / 2;
   Queue lhs_q(s), rhs_q(s);
 
+  lhs_q.init_nob();
+  rhs_q.init_nob();
+
   size_t i, j;
   int32_t lhs_val, rhs_val, out_val;
+
+  int32_t ob_write = 0, lhs_out_idx = 0, rhs_out_idx = 0, arr_out_idx = 0;
+  int32_t max_ob_write = 1;  // TODO: API to get max ob write number
 
   coda_txbegin();
 
   for (i = 0; i < half_s; ++i) {
     if (i < lhs_len) {
       lhs_q.enqueue(ob_read_next(lhs_out_ob));
+      lhs_out_idx++;
     }
     if (i < rhs_len) {
       rhs_q.enqueue(ob_read_next(rhs_out_ob));
+      rhs_out_idx++;
     }
   }
   for (; i < rhs_len + half_s; ++i) {
     if (i < lhs_len) {
       lhs_q.enqueue(ob_read_next(lhs_out_ob));
+      lhs_out_idx++;
     }
     if (i < rhs_len) {
       rhs_q.enqueue(ob_read_next(rhs_out_ob));
+      rhs_out_idx++;
     }
 
     for (j = 0; j < 2; ++j) {
+      if (ob_write >= max_ob_write) {
+        reset_tx(&lhs_q, &rhs_q, lhs_out, rhs_out, lhs_len, rhs_len,
+                 lhs_out_idx, rhs_out_idx, arr_out, len, arr_out_idx,
+                 &lhs_out_ob, &rhs_out_ob, &arr_out_ob, &ob_write,
+                 &max_ob_write);
+      }
+
       if (lhs_q.get_cur_len() > 0 && rhs_q.get_cur_len() > 0) {
         lhs_val = lhs_q.front();
         rhs_val = rhs_q.front();
@@ -143,6 +194,8 @@ void merge_sort(const int32_t* arr_in, int32_t* arr_out, const size_t len)
           rhs_q.dequeue();
         }
         ob_rw_write_next(arr_out_ob, out_val);
+        arr_out_idx++;
+        ob_write++;
       } else {
         if (i < lhs_len || i < rhs_len) abort();
         goto done;
@@ -155,20 +208,35 @@ void merge_sort(const int32_t* arr_in, int32_t* arr_out, const size_t len)
 done:
 
   while (lhs_q.get_cur_len() > 0) {
+    if (ob_write >= max_ob_write) {
+      reset_tx(&lhs_q, &rhs_q, lhs_out, rhs_out, lhs_len, rhs_len, lhs_out_idx,
+               rhs_out_idx, arr_out, len, arr_out_idx, &lhs_out_ob, &rhs_out_ob,
+               &arr_out_ob, &ob_write, &max_ob_write);
+    }
     out_val = lhs_q.front();
     lhs_q.dequeue();
     ob_rw_write_next(arr_out_ob, out_val);
+    arr_out_idx++;
+    ob_write++;
   }
 
   while (rhs_q.get_cur_len() > 0) {
+    if (ob_write >= max_ob_write) {
+      reset_tx(&lhs_q, &rhs_q, lhs_out, rhs_out, lhs_len, rhs_len, lhs_out_idx,
+               rhs_out_idx, arr_out, len, arr_out_idx, &lhs_out_ob, &rhs_out_ob,
+               &arr_out_ob, &ob_write, &max_ob_write);
+    }
     out_val = rhs_q.front();
     rhs_q.dequeue();
     ob_rw_write_next(arr_out_ob, out_val);
+    arr_out_idx++;
+    ob_write++;
   }
 
   coda_txend();
 
-  for (int i = 0; i < len; i++) arr_out[i] = ob_rw_read_next(arr_out_ob);
+  for (int i = 0; i < ob_write; i++)
+    arr_out[arr_out_idx - ob_write + i] = ob_rw_read_next(arr_out_ob);
 
   delete[] lhs_out;
   delete[] rhs_out;
