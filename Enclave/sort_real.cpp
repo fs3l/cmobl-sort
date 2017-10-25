a0f90ef75a3717433abc6d36de774a08d02fde24
Xu Cheng <git@xuc.me>
2017-10-25 17:41:32 -0400

    cleanup code

--- Enclave/sort_real.cpp
+++ Enclave/sort_real.cpp
@@ -10,12 +10,9 @@
 #include <stdlib.h>
 
 #include "./Enclave.h"
-#include "./sort.h"
 #include "./RealLibCoda.h"
+#include "./sort.h"
 #define C 4
-int a[10] = {99,96,1,2,3,100,56,24,78,21};
-
-void print_arr(const int* arr, const int len);
 
 class Queue
 {
@@ -26,36 +23,28 @@ public:
     data[0] = max_len;
     data[1] = 0;
     data[2] = 0;
-    //nob = coda->make_nob_array(data, max_len + 3);
     nob = initialize_nob_array(data, max_len + 3);
   }
-  ~Queue()
-  {
-    delete[] data;
-  }
-
+  ~Queue() { delete[] data; }
   __attribute__((always_inline)) inline int32_t get_max_len() const
   {
-    //return nob->read_at(0);
-    return nob_read_at(nob,0);
+    return nob_read_at(nob, 0);
   }
   __attribute__((always_inline)) inline int32_t get_cur_len() const
   {
-    //return nob->read_at(1);
-    return nob_read_at(nob,1);
+    return nob_read_at(nob, 1);
   }
   __attribute__((always_inline)) inline int32_t get_front_idx() const
   {
-    //return nob->read_at(2);
-    return nob_read_at(nob,2);
+    return nob_read_at(nob, 2);
   }
   __attribute__((always_inline)) inline void set_cur_len(int32_t cur_len)
   {
-    nob_write_at(nob,1,cur_len);
+    nob_write_at(nob, 1, cur_len);
   }
   __attribute__((always_inline)) inline void set_front_idx(int32_t front_idx)
   {
-    nob_write_at(nob,2, front_idx);
+    nob_write_at(nob, 2, front_idx);
   }
 
   __attribute__((always_inline)) inline void enqueue(const int32_t val)
@@ -64,7 +53,7 @@ public:
     int32_t cur_len = get_cur_len();
     int32_t front_idx = get_front_idx();
     if (cur_len == max_len) abort();
-    nob_write_at(nob,(front_idx + cur_len) % max_len + 3, val);
+    nob_write_at(nob, (front_idx + cur_len) % max_len + 3, val);
     ++cur_len;
     set_cur_len(cur_len);
   }
@@ -73,7 +62,7 @@ public:
     int32_t cur_len = get_cur_len();
     int32_t front_idx = get_front_idx();
     if (cur_len == 0) abort();
-    return nob_read_at(nob,front_idx + 3);
+    return nob_read_at(nob, front_idx + 3);
   }
   __attribute__((always_inline)) inline void dequeue()
   {
@@ -106,8 +95,6 @@ void merge_sort(const int32_t* arr_in, int32_t* arr_out, const size_t len)
   int32_t* rhs_out = new int32_t[rhs_len];
   merge_sort(arr_in, lhs_out, lhs_len);
   merge_sort(arr_in + lhs_len, rhs_out, rhs_len);
-
-  // TODO: replace new Coda() to proper implementation.
 
   HANDLE lhs_out_ob = initialize_ob_iterator(lhs_out, lhs_len);
   HANDLE rhs_out_ob = initialize_ob_iterator(rhs_out, rhs_len);
@@ -155,7 +142,7 @@ void merge_sort(const int32_t* arr_in, int32_t* arr_out, const size_t len)
           out_val = rhs_val;
           rhs_q.dequeue();
         }
-        ob_rw_write_next(arr_out_ob,out_val);
+        ob_rw_write_next(arr_out_ob, out_val);
       } else {
         if (i < lhs_len || i < rhs_len) abort();
         goto done;
@@ -170,23 +157,21 @@ done:
   while (lhs_q.get_cur_len() > 0) {
     out_val = lhs_q.front();
     lhs_q.dequeue();
-    ob_rw_write_next(arr_out_ob,out_val);
+    ob_rw_write_next(arr_out_ob, out_val);
   }
 
   while (rhs_q.get_cur_len() > 0) {
     out_val = rhs_q.front();
     rhs_q.dequeue();
-    ob_rw_write_next(arr_out_ob,out_val);
+    ob_rw_write_next(arr_out_ob, out_val);
   }
 
   coda_txend();
 
-  for(int i=0;i<len;i++)
-    arr_out[i] = ob_rw_read_next(arr_out_ob);
+  for (int i = 0; i < len; i++) arr_out[i] = ob_rw_read_next(arr_out_ob);
 
   delete[] lhs_out;
   delete[] rhs_out;
-
 }
 
 int* random_arr(const int len)
@@ -211,12 +196,13 @@ void print_arr(const int* arr, const int len)
 
 int sort(int len)
 {
-//  int32_t* a = random_arr(len);
+  // int32_t* a = random_arr(len);
+  int32_t a[10] = {99, 96, 1, 2, 3, 100, 56, 24, 78, 21};
   print_arr(a, len);
   int32_t* b = new int32_t[len];
   merge_sort(a, b, len);
   print_arr(b, len);
-  //delete[] a;
+  // delete[] a;
   delete[] b;
   return 0;
 }
